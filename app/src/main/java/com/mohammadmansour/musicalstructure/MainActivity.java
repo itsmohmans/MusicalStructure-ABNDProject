f96493dee05b8b99a0e64d1a94bf1620a0027b29
Mohammad Mansour <55551984+itsmohmans@users.noreply.github.com>
2020-11-24 14:26:31 +0200

    create NowPlayingActivity and design activity_now_playing

--- app/src/main/java/com/mohammadmansour/musicalstructure/MainActivity.java
+++ app/src/main/java/com/mohammadmansour/musicalstructure/MainActivity.java
@@ -1,20 +1,31 @@
 package com.mohammadmansour.musicalstructure;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @BindView(R.id.BUTTON_ID)
-//    Button btn;
+    @BindView(R.id.now_playing_footer)
+    LinearLayout nowPlayingFooter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
+
+        nowPlayingFooter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, NowPlaying.class);
+                startActivity(intent);
+            } });
     }
 }
